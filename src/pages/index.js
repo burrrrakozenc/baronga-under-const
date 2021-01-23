60b01b45a3624b6dee21d4a261fd26f69cf8b5d5
burrrrakozenc <brk.ozenc@gmail.com>
2021-01-23 21:38:32 +0300

    commit againe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import '../styles/style.css'
+import Logo from '../image/Amblem.png'
+import { Helmet } from "react-helmet"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className="page">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Neferka Design</title>
+      <link rel="canonical" href="http://neferka.design" />
+    </Helmet>
+    <img className="img" src={Logo} alt="Neferka" />
+    <h2 className="h2">UNDER CONSTRUCTION</h2>
+  </div>
 }
