78b355525b557a658be0b553c3820cd4da3aed7d
burrrrakozenc <brk.ozenc@gmail.com>
2021-01-24 00:05:23 +0300

    commit againe

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,21 @@ export default function Home() {
       <title>Neferka Design</title>
       <link rel="canonical" href="http://neferka.design" />
     </Helmet>
-    <img className="img" src={Logo} alt="Neferka" />
-    <h2 className="h2">UNDER CONSTRUCTION</h2>
+    <img className="image" src={Logo} alt="Neferka" />
+    <h2 className="header">UNDER CONSTRUCTION</h2>
+    <div className="download-content">
+      <p><a href="https://drive.google.com/file/d/1G6j7sGwrvidxdrPUqkY1-mJmcEfYtKnL/view?usp=sharing" rel="noreferrer" className="download-content">Download </a>our catalogue. For inquiries email eli@neferka.design</p>
+      {/* <p className="communication">For inquiries email eli@neferka.design</p> */}
+    </div>
+    <div className="footer"><p>
+      <a target="_blank" rel="noreferrer" href="https://www.facebook.com/neferkadesign">
+        FACEBOOK
+      </a>
+      &nbsp;&nbsp;/&nbsp;&nbsp;
+      <a target="_blank" rel="noreferrer" href="https://www.instagram.com/neferka_design/">
+        INSTAGRAM
+      </a>
+    </p>
+    </div>
   </div>
 }
